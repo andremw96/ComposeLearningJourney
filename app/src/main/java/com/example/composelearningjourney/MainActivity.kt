d29fd174de5abd578986fd87b4a744a2d2e0cdb9
andremw96 <andre19961@gmail.com>
2022-01-28 10:02:29 +0700

    add button to app module, can open meditation UI and animation codelab

--- app/src/main/java/com/example/composelearningjourney/MainActivity.kt
+++ app/src/main/java/com/example/composelearningjourney/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.composelearningjourney
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.composelearningjourney.ui.theme.ComposeLearningJourneyTheme
+import com.example.android.codelab.animation.MainActivity as AnimationActivity
+import com.plcoding.meditationuiyoutube.MainActivity as MeditationActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        ButtonGoTo("Meditation UI with Compose") {
+                            startActivity(Intent(
+                                this@MainActivity, MeditationActivity::class.java
+                            ))
+                        }
+                        
+                        Spacer(modifier = Modifier.padding(8.dp))
+
+                        ButtonGoTo("Animation With Compose") {
+                            startActivity(Intent(
+                                this@MainActivity, AnimationActivity::class.java
+                            ))
+                        }
+                    }
                 }
             }
         }
@@ -30,14 +54,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeLearningJourneyTheme {
-        Greeting("Android")
+fun ButtonGoTo(name: String, onClick: () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Text(text = "Go To Module $name", textAlign = TextAlign.Center)
     }
 }
